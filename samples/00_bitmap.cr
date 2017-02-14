18c076362622db90778673acf7bda191debc0887
Michael Prins <michael@leap.ly>
2017-02-15 00:43:39 +0200

    Rename bmp.cr and add explanatory comments to it

--- samples/00_bitmap.cr
+++ samples/00_bitmap.cr
@@ -1,14 +1,20 @@
 require "../sdl"
 
+# Initialize a SDL context for rendering video and ensure it closes when done
 SDL.init(SDL::Init::VIDEO)
 at_exit { SDL.quit }
 
+# Create a SDL::Window that we can draw to
 window = SDL::Window.new("SDL tutorial", 640, 480)
+# Create a bitmap image buffer in memory from a file
 bmp = SDL.load_bmp(File.join(__DIR__, "data", "hello_world.bmp"))
 
+# Keep the application running until this loop exits
 loop do
+  # Check for interaction from the user
   event = SDL::Event.wait
 
+  # Break the loop if user wants to quit
   case event
   when SDL::Event::Quit
     break
@@ -18,6 +24,7 @@ loop do
     end
   end
 
+  # Otherwise blit (copy) the image buffer to the window and show it
   bmp.blit(window.surface)
   window.update
 end
