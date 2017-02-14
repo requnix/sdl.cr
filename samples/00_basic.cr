78ee43fc821fe5680f1a11e17a885549ec988f63
Michael Prins <michael@leap.ly>
2017-02-15 00:42:50 +0200

    Rename basic.cr and add explanatory comments to it

--- samples/00_basic.cr
+++ samples/00_basic.cr
@@ -1,13 +1,18 @@
 require "../sdl"
 
+# Initialize a SDL context for rendering video and ensure it closes when done
 SDL.init(SDL::Init::VIDEO)
 at_exit { SDL.quit }
 
+# Create a SDL::Window that we can draw to
 window = SDL::Window.new("SDL tutorial", 640, 480)
 
+# Keep the application running until this loop exits
 loop do
+  # Check for interaction from the user
   event = SDL::Event.wait
 
+  # Break the loop if user wants to quit
   case event
   when SDL::Event::Quit
     break
@@ -17,6 +22,7 @@ loop do
     end
   end
 
+  # Otherwise cover the window surface with white and show it
   window.surface.fill(255, 255, 255)
   window.update
 end
